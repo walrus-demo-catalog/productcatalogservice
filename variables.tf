6f3063e0021a899bcd70d21408158aea29733c52
Yinlin Li <yinlin@seal.io>
2023-11-27 13:13:39 +0800

    Add schema, Fix replicas & Optimize walrus runtime context

--- variables.tf
+++ variables.tf
@@ -1,29 +1,21 @@
-# @group "Kubernetes Configuration"
-# @label "Name"
 variable "name" {
   type        = string
   description = "Name of the deployment resource. Auto-generated if empty."
   default     = ""
 }
 
-# @group "Kubernetes Configuration"
-# @label "Namespace"
 variable "namespace" {
   type        = string
   description = "Namespace to deploy. Auto-generated if empty."
   default     = ""
 }
 
-# @group "Kubernetes Configuration"
-# @label "Replicas"
 variable "replicas" {
   type        = number
   description = "Replicas to deploy"
   default     = 1
 }
 
-# @group "Image Configuration"
-# @label "Image Registry"
 variable "image_registry" {
   description = "Image registry"
   type        = string
@@ -31,8 +23,6 @@ variable "image_registry" {
 }
 
 
-# @group "Image Configuration"
-# @label "Image Repository"
 variable "image_repository" {
   description = "Image repository"
   type        = string
@@ -40,34 +30,18 @@ variable "image_repository" {
 }
 
 
-# @group "Image Configuration"
-# @label "Image Tag"
 variable "image_version" {
   description = "Image tag"
   type        = string
   default     = "v0.8.0"
 }
 
-# @hidden
 variable "service_name" {
   type        = string
   description = "Service name"
   default     = "productcatalogservice"
 }
 
-# @hidden
-variable "walrus_metadata_service_name" {
-  type        = string
-  description = "Walrus metadata service name."
-  default     = ""
-}
-# @hidden
-variable "walrus_metadata_namespace_name" {
-  type        = string
-  description = "Walrus metadata namespace name."
-  default     = ""
-}
-# @hidden
 variable "context" {
   description = <<-EOF
 Receive contextual information. When Walrus deploys, Walrus will inject specific contextual information into this field.
